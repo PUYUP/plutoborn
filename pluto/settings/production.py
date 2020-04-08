362038c8af11d8b78298a6fad1fc4c96348f843b
PUYUP <hellopuyup@gmail.com>
2020-04-08 23:13:04 +0700

    Remove STATICFILES_STORAGE

--- pluto/settings/production.py
+++ pluto/settings/production.py
@@ -47,7 +47,7 @@ MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 
 # Add configuration for static files storage using whitenoise
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Database

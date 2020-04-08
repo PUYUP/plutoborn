19713fef6f084840982b571dc20dfb65e0d1558f
PUYUP <hellopuyup@gmail.com>
2020-04-09 06:25:41 +0700

    Add end slash

--- pluto/settings/base.py
+++ pluto/settings/base.py
@@ -60,7 +60,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(PROJECT_PATH, 'templates')
+            os.path.join(PROJECT_PATH, 'templates/')
         ],
         'APP_DIRS': True,
         'OPTIONS': {

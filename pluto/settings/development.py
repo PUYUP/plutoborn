576dc1e25561d77b59d2794519437766ba2dd5b2
PUYUP <hellopuyup@gmail.com>
2020-04-08 23:09:43 +0700

    Add admin static file

--- pluto/settings/development.py
+++ pluto/settings/development.py
@@ -38,8 +38,7 @@ CSRF_TRUSTED_ORIGINS = []
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, 'static/'),
-)
--- pluto/settings/production.py
+++ pluto/settings/production.py
@@ -47,6 +47,7 @@ MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 
 # Add configuration for static files storage using whitenoise
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Database

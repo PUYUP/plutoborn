b48be1542b79527f7178122729720bbb19ca1e17
PUYUP <hellopuyup@gmail.com>
2020-08-13 08:03:44 +0700

    change settings

--- pluto/settings/__init__.py
+++ pluto/settings/__init__.py
@@ -3,7 +3,7 @@ from .project import *
 
 # check setting load from live server
 # this time all live server mark as production grade
-if os.environ.get('PRODUCTION', False) or sys.platform == 'linux' or sys.platform == 'linux2':
+if sys.platform == 'linux':
     from .production import *
 else:
     from .development import *

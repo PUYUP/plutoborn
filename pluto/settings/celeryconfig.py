7d9b8d4343783e22bbd927a74ee8110d7e0b5f13
PUYUP <hellopuyup@gmail.com>
2020-08-16 16:34:29 +0700

    Update celery

--- pluto/settings/celeryconfig.py
+++ pluto/settings/celeryconfig.py
@@ -1,6 +1,10 @@
 from django.conf import settings
 
-broker_url = settings.REDIS_URL
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = '6379'
+REDIS_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+
+broker_url = REDIS_URL
 broker_transport_options = {'visibility_timeout': 3600} 
-result_backend = settings.REDIS_URL
+result_backend = REDIS_URL
 task_serializer = 'json'
